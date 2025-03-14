97983e0daca2f4b133fbcf13c68930e9439e10fe
dinakajoy <odinakajoy909@gmail.com>
2025-03-14 19:41:56 +0100

    fix cors

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import Cors from 'nextjs-cors';
 import { ApolloServer } from "apollo-server-micro";
 import typeDefs from "../../db/graphql/quiz.types";
 import resolvers from "../../db/graphql/quiz.resolvers";
@@ -9,6 +10,12 @@ const apolloServer = new ApolloServer({ typeDefs, resolvers });
 const startServer = apolloServer.start();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await Cors(req, res, {
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    origin: 'https://quiz-app-graphql-api.vercel.app',
+    optionsSuccessStatus: 200,
+  });
+
   if (req.method === 'OPTIONS') {
     res.end()
     return false
